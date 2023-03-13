1d1f0f3b47d3dc0153f5ee71cca96b64897139c4
atrufanov <atryfv@gmail.com>
2023-03-13 17:25:03 +0300

    Выгружена таблица из гугл

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import folium
 import pandas as pd
 import os
 import requests
+import gspread as gs
 
-sheet_id = "10awekJr1rw0AikidtV5rVTpM0AI1zjwUcodCp5Q3SXE"
-sheet_name = "Лист7"
-url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
 
-url = url
-df = pd.read_csv(url)
-# print (url)
+gc = gs.service_account(filename='model-zoo-363409-d778a0fccb80.json')
+sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/10awekJr1rw0AikidtV5rVTpM0AI1zjwUcodCp5Q3SXE/edit#gid=1313466868')
+
+ws = sh.worksheet('Лист7')
+
+df = pd.DataFrame(ws.get_all_records())
+print(list(df))
